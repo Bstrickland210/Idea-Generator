88dc4b6a2b169e31fe12f3c6f4467812f56502a9
Bridger Strickland <bridger210strick@gmail.com>
2022-02-03 13:09:00 -0700

    Added timer

--- Idea Generator/ViewController.swift
+++ Idea Generator/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController
     let wordideas = ["Graphic Novel", "music", "Role play", "plan a party", "peer assesment", "podcast","design a business", "gallary walk", "research", "debate", "mentor feedback", "plan a festival", "exeplar work", "articles", "write and produce a play", "pop up book", "design challenge", "interviews", "simulation or game", "guest speaker", "film festival", "grows and glows", "nature journal", "film clips", "mobile app", "art installation", "literature circles", "oral history", "puppet show", "maps", "community inventory", "field investigation", "theme park", "documentary", "reflection journal", "goal setting", "lab investigation", "photo essay", "texted based discussion", "expert mentors", "walking tour", "discrepent events", "books", "wild card", "add campain", "short stories", "community garden", "ted talks", "seminar disscuions", "facility tour", "design a memorial", "billboards", "museum exhibit", "quick write", "expert topics", "jigsaw"]
     
     
+    @IBOutlet weak var generateButtonOutlet: UIButton!
     
     
     @IBAction func generateButton(_ sender: Any)
@@ -71,6 +72,11 @@ class ViewController: UIViewController
             else
             {
             x = 1
+            }
+            generateButtonOutlet.isEnabled = false
+            Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false)
+                {timer in
+                self.generateButtonOutlet.isEnabled = true
             }
         }
         

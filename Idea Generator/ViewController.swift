012f31b04343f921d4552218b502a437d11371a4
Bridger Strickland <bridger210strick@gmail.com>
2022-02-09 11:54:48 -0700

    hide second button

--- Idea Generator/ViewController.swift
+++ Idea Generator/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController
     @IBOutlet weak var wordFour: UILabel!
     var x = 0
     
+    @IBOutlet weak var buttonTwo: UIButton!
+    
     let wordideas = ["Graphic Novel", "music", "Role play", "plan a party", "peer assesment", "podcast","design a business", "gallary walk", "research", "debate", "mentor feedback", "plan a festival", "exeplar work", "articles", "write and produce a play", "pop up book", "design challenge", "interviews", "simulation or game", "guest speaker", "film festival", "grows and glows", "nature journal", "film clips", "mobile app", "art installation", "literature circles", "oral history", "puppet show", "maps", "community inventory", "field investigation", "theme park", "documentary", "reflection journal", "goal setting", "lab investigation", "photo essay", "texted based discussion", "expert mentors", "walking tour", "discrepent events", "books", "wild card", "add campain", "short stories", "community garden", "ted talks", "seminar disscuions", "facility tour", "design a memorial", "billboards", "museum exhibit", "quick write", "expert topics", "jigsaw"]
     
     
@@ -96,7 +98,7 @@ class ViewController: UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
+        buttonTwo.isHidden = true 
 
     }
 }

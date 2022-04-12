0433753c373d456d40415a688faf4081e350ea23
Jackson <jhhemme43@gmail.com>
2022-04-12 11:23:51 -0600

    added new screen

--- Idea Generator/ViewController.swift
+++ Idea Generator/ViewController.swift
@@ -106,7 +106,7 @@ class ViewController: UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        buttonTwo.isHidden = true 
+      //  buttonTwo.isHidden = true
 
     }
 }
